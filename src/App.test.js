9b474fa0f7468663040b835372e3844169e38569
uc-creat <utkarshchauhanutc26@gmail.com>
2023-06-15 12:22:46 +0530

    demo commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Time/i);
+  const linkElement = screen.getByText(/somethingnew/i);
   expect(linkElement).toBeInTheDocument();
 });
